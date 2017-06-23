585aae8ce158d1e7cbd63be9b44d251427dbfc04
cgkim <cgkim@captv.co.kr>
2017-06-23 13:38:23 +0900

    fix test hello pooq

--- app/test2.py
+++ app/test2.py
@@ -7,7 +7,7 @@ class BasicTestCase(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.data, b'Hello World')
+        self.assertEqual(response.data, b'Hello pooq')
 
 if __name__ == '__main__':
     unittest.main()
